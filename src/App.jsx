9f2ff20893186901c0a63ffdfae0eeddc12511a0
sj <sijiaw@yahoo.com>
2022-03-03 08:03:28 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,167 @@
+import React, { useEffect, useState } from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
+import './App.css';
+
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const TEST_GIFS = [
+	'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
+
+// 'https://open.spotify.com/embed/track/3uouaAVXpQR3X8RYkJyitQ',
+	// 'https://open.spotify.com/embed/track/6N22FZs2ZhPBYi3b9XPajV',
+	// 'https://open.spotify.com/embed/track/45S5WTQEGOB1VHr1Q4FuPl',
+	// 'https://open.spotify.com/embed/track/4SSnFejRGlZikf02HLewEF'
+
+const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
+
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+
+          /*
+           * Set the user's publicKey in state to be used later!
+           */
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+ const connectWallet = async () => {
+  const { solana } = window;
+
+  if (solana) {
+    const response = await solana.connect();
+    console.log('Connected with Public Key:', response.publicKey.toString());
+    setWalletAddress(response.publicKey.toString());
+  }
+};
+
+  const sendGif = async () => {
+  if (inputValue.length > 0) {
+    console.log('Gif link:', inputValue);
+    setGifList([...gifList, inputValue]);
+    setInputValue('');
+  } else {
+    console.log('Empty input. Try again.');
+  }
+};
+
+  const onInputChange = (event) => {
+  const { value } = event.target;
+  setInputValue(value);
+};
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter gif link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="cta-button submit-gif-button">
+          let's go
+        </button>
+      </form>
+      <div className="gif-grid">
+        {/* Map through gifList instead of TEST_GIFS */}
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  // <iframe src={gif} scrolling="yes" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  
+
+  // UseEffects
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  useEffect(() => {
+  if (walletAddress) {
+    console.log('Fetching GIF list...');
+    
+    // Call Solana program here.
+
+    // Set state
+    setGifList(TEST_GIFS);
+  }
+}, [walletAddress]);
+  
+
+  return (
+  <div className="App">
+    <div className="container">
+      <div className="header-container">
+        <p className="header">🖼 GIF Portal</p>
+        <p className="sub-text">
+          View your GIF collection in the metaverse ✨
+        </p>
+        {!walletAddress && renderNotConnectedContainer()}
+        {/* We just need to add the inverse here! */}
+        {walletAddress && renderConnectedContainer()}
+      </div>
+      <div className="footer-container">
+        <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <a
+          className="footer-text"
+          href={TWITTER_LINK}
+          target="_blank"
+          rel="noreferrer"
+        >{`built on @${TWITTER_HANDLE}`}</a>
+      </div>
+    </div>
+  </div>
+);
+};
+
+export default App;
